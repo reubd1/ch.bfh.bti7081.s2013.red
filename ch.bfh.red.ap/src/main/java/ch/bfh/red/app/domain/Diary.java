e8e985f164f71ee77027d8a9d68086d88c772d2d
paras1 <paras1@bfh.ch>
2013-05-13 22:20:33 +0200

    rename -> ch.bfh.red.app to ch.bfh.red.ap to clean directory

---
+++ ch.bfh.red.ap/src/main/java/ch/bfh/red/app/domain/Diary.java
@@ -0,0 +1,57 @@
+package ch.bfh.red.app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 24)
+    private String topic;
+    private String entry;
+    
+    private String dateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getEntry() {
+		return entry;
+	}
+
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+
+    
+}
