4724dd5e961a7d32a29ccac0e92281705ea03aa0
stola <stola@schleppi>
2013-05-21 16:56:27 +0200

    checkin for paras1

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/RedAppUI.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/RedAppUI.java
@@ -33,7 +33,7 @@ public class RedAppUI extends UI {
 
 		diaries = JPAContainerFactory.make(Diary.class,
 				JpaAddressbookUI.PERSISTENCE_UNIT);
-		
+
 		// Set the window or tab title
 		getPage().setTitle("Welcome RedApp");
 
@@ -41,34 +41,36 @@ public class RedAppUI extends UI {
 		layout.setMargin(true);
 		setContent(layout);
 
-		
 		final BeanItem<Diary> newDiaryItem = new BeanItem<Diary>(new Diary());
-		
+
 		DiaryEditor diaryEditor = new DiaryEditor(newDiaryItem);
 		diaryEditor.setWidth(800, Unit.PIXELS);
 		diaryEditor.setHeight(500, Unit.PIXELS);
-		
-
 
 		diaryEditor.addListener(new EditorSavedListener() {
 			private static final long serialVersionUID = -4810596568407523252L;
 
 			@Override
 			public void editorSaved(EditorSavedEvent event) {
-				// get and set current DateTime 
-				newDiaryItem.getBean().setDateTime( Calendar.getInstance());
+				// get and set current DateTime
+				newDiaryItem.getBean().setDateTime(Calendar.getInstance());
 
 				// write some "logs"
 				System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 				System.out.println("------------" + newDiaryItem.getBean().getEntry());
 				System.out.println("------------" + newDiaryItem.getBean().getFeeling());
-				
+
 				diaries.addEntity(newDiaryItem.getBean());
 
 			}
 		});
 
 		layout.addComponent(diaryEditor);
+		
+		DiarySummaryView diarySummaryView = new DiarySummaryView(newDiaryItem);
+		diarySummaryView.setWidth(800, Unit.PIXELS);
+		diarySummaryView.setHeight(500, Unit.PIXELS);
+		layout.addComponent(diarySummaryView);
 
 	}
 }
