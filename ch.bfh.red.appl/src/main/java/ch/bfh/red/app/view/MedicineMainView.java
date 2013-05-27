c060fc8f2c852e5a4e5a37d2c999e4b56fac9fd6
paras1 <paras1@bfh.ch>
2013-05-27 16:28:59 +0200

    TouchKit compatibility

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/MedicineMainView.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/MedicineMainView.java
@@ -15,26 +15,32 @@
 */
 package ch.bfh.red.app.view;
 
+import ch.bfh.red.app.controller.DiaryEditor;
+import ch.bfh.red.app.model.assignment.Diary;
 import ch.bfh.red.app.model.assignment.Medication;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.JPAContainerFactory;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.addon.touchkit.ui.Popover;
 import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.VerticalSplitPanel;
 
-public class MedicineMainView extends VerticalSplitPanel implements
-		ComponentContainer {
+public class MedicineMainView extends NavigationView implements ClickListener {
 
 	/**
 	 * 
 	 */
-
+	private static final long serialVersionUID = 1276328592221877100L;
+	
 	private Label lbTitle;
 	private Button bConsumeTracking;
 	private Button bStockMgmt;
@@ -45,45 +51,49 @@ public class MedicineMainView extends VerticalSplitPanel implements
 	
 	public MedicineMainView(Item medicationItem) {
 		this.medicationItem = medicationItem;	
-		medication = JPAContainerFactory.make(Medication.class,
-				JpaAddressbookUI.PERSISTENCE_UNIT);
-		buildMainArea();
+		medication = JPAContainerFactory.make(Medication.class, JpaAddressbookUI.PERSISTENCE_UNIT);
 
-		setSplitPosition(30);
+
 	}
+	
+    public void attach() {
+        super.attach();
+        
+            buildView();
+    }
 
-	private void buildMainArea() {
+	private void buildView() {
+		VerticalLayout verticalLayoutGround = new VerticalLayout();
 		VerticalLayout verticalLayoutTop = new VerticalLayout();
 		VerticalLayout verticalLayoutBottom = new VerticalLayout();
-		setFirstComponent(verticalLayoutTop);
-		setSecondComponent(verticalLayoutBottom);
+
 
 		lbTitle = new Label("Medikamente");
 		lbTitle.setImmediate(true);
 		lbTitle.setSizeFull();
 
 		bConsumeTracking = new Button("Einnahme Einsicht");
-	    bConsumeTracking.addClickListener(new Button.ClickListener() {
-
-	            public void buttonClick(ClickEvent event) {
-
-//	                UI.getCurrent().setNavigator(Navigator)
-	            }
-	    });
 		
 	    bStockMgmt = new Button("Vorrat Verwalten");
-	    bStockMgmt.addClickListener(new Button.ClickListener() {
 
-                public void buttonClick(ClickEvent event) {
-
-//                  UI.getCurrent().setNavigator(Navigator)
-                }
-        });
 
 		verticalLayoutTop.addComponent(lbTitle);
 		verticalLayoutTop.setExpandRatio(lbTitle, 1);
 		verticalLayoutBottom.addComponent(bConsumeTracking);
 		verticalLayoutBottom.addComponent(bStockMgmt);
+		
+		verticalLayoutGround.addComponent(verticalLayoutTop);
+		verticalLayoutGround.addComponent(verticalLayoutBottom);
 
 	}
+	
+	public void buttonClick(ClickEvent event) {
+//		if (bConsumeTracking == event.getButton()) {
+//            Popover popover = new Popover();
+//            popover.setSizeFull();
+//            popover.setModal(false);
+//            popover.setClosable(true);
+//            popover.setContent();
+//            UI.getCurrent().addWindow(popover);
+	} 
 }
