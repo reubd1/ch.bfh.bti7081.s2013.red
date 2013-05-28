0340a1aadd713fb9428aec7dd841611538adc647
Adrian Bärtschi <baertschi.adrian@gmail.com>
2013-05-28 20:36:02 +0200

    Cleanup

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/DiarySummaryView.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/view/DiarySummaryView.java
@@ -1,17 +1,6 @@
 /**
- * Copyright 2009-2013 Oy Vaadin Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * View with a list of all diary entries
+ * @author barta3
  */
 package ch.bfh.red.app.view;
 
@@ -20,34 +9,23 @@ import ch.bfh.red.app.model.assignment.Diary;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.JPAContainerFactory;
-import com.vaadin.addon.touchkit.ui.NavigationButton;
 import com.vaadin.addon.touchkit.ui.NavigationView;
-import com.vaadin.addon.touchkit.ui.Popover;
-import com.vaadin.data.Item;
 import com.vaadin.data.util.BeanItem;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.UI;
 
 public class DiarySummaryView extends NavigationView {
 
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1276328592221877100L;
 
 	private Table diaryEntriesTable;
 
-
 	private JPAContainer<Diary> diaries;
 
-	
 	final BeanItem<Diary> newDiaryItem = new BeanItem<Diary>(new Diary());
-	
 	
 	public DiarySummaryView() {
 		diaries = JPAContainerFactory.make(Diary.class,	RedAppUI.PERSISTENCE_UNIT);
