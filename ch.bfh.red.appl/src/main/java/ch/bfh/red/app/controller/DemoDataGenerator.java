a8689e46dd4691b84e3ff51e8eac8636cbe51c69
stola <stola@schleppi>
2013-05-31 14:21:38 +0200

    Update Testdata

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/controller/DemoDataGenerator.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/controller/DemoDataGenerator.java
@@ -7,51 +7,76 @@ import javax.persistence.Persistence;
 
 import ch.bfh.red.app.model.assignment.Medication;
 import ch.bfh.red.app.model.assignment.Medication.DosisUnitEnum;
+import ch.bfh.red.app.model.assignment.Medicine;
 import ch.bfh.red.app.model.profile.Patient;
 
 public class DemoDataGenerator {
 
-	final static String[] medis = { "Riatlin", "Morphium", "UML4ever", "IT",
-			"Quality Assurance", "Research and Development", "Production" };
+	final static String[] medis = { "Riatlin", "Morphium", "UML4ever", "IT", "Quality Assurance", "Research and Development",
+			"Production" };
 
-	final static String[] fnames = { "Erich", "Pierre", "Olivier", "Mario",
-			"Jan", "Guido" };
-	final static String[] lnames = { "Badertscher", "Fierz", "Büchel", "Super",
-			"Locher", "Bucher" };
+	final static String[] fnames = { "Erich", "Pierre", "Olivier", "Mario", "Jan", "Guido" };
+	final static String[] lnames = { "Badertscher", "Fierz", "Büchel", "Super", "Locher", "Bucher" };
 	final static String cities[] = { "Bern", "Burgdorf", "Biel", "Neverland" };
 
+	private static EntityManager em = null;
 
-	public static void create() {
+	public static void initDemoData() {
 
-		EntityManager em = Persistence.createEntityManagerFactory("redapp")
-				.createEntityManager();
+		em = Persistence.createEntityManagerFactory("redapp").createEntityManager();
 
 		// Test: filling bean with the id of a patient
+		createPatient();
 
+		createMedicine();
+		createMedicaiton();
+
+	}
+
+	private static void createPatient() {
 		Patient patient = new Patient();
 		patient.setFirstname("Bluberio");
 		patient.setName("Bluber");
+		patient.setCity("Bern");
 		patient.setIndependenceLevel(2);
 
 		em.getTransaction().begin();
 		em.persist(patient);
 		em.getTransaction().commit();
-		
-		
-//		em.getTransaction().begin();
-//		Random r = new Random(0);
-//		int i = 0;
-//		for (String cM : medis) {
-//				Medication m = new Medication();
-//				m.setEntry(cM);
-//				m.setDosisUnit(DosisUnitEnum.values()[r.nextInt(DosisUnitEnum.values().length)]);
-//				m.setDosis((i * i) + 1L);
-//				
-//				em.persist(m);
-//				i++;
-//		}
-//
-//		em.getTransaction().commit();
 	}
 
+	private static void createMedicine() {
+		em.getTransaction().begin();
+		Random r = new Random(0);
+		for (String cM : medis) {
+			// Medicine
+			Medicine m = new Medicine();
+
+			m.setName(cM);
+			m.setDescription(cM + " Beschreibung");
+
+			em.persist(m);
+		}
+
+		em.getTransaction().commit();
+
+	}
+
+	private static void createMedicaiton() {
+		em.getTransaction().begin();
+
+		Medication m = new Medication();
+
+		Medicine medicine = em.find(Medicine.class, new Long(2));
+
+		m.setMedicine(medicine);
+		m.setDosis(10L);
+		m.setDosisUnit(DosisUnitEnum.miligramm);
+		m.setIntervalInHours(6L);
+		m.setStock(10L);
+
+		em.persist(m);
+		
+		em.getTransaction().commit();
+	}
 }
