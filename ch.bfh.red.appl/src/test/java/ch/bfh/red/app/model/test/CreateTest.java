81411e1d67ef7f44d8e5828082f49fd6c8f556d6
reubd1 <reubd1@dell-ubuntu-domi>
2013-06-09 22:20:30 +0200

    changed

--- ch.bfh.red.appl/src/test/java/ch/bfh/red/app/model/test/CreateTest.java
+++ ch.bfh.red.appl/src/test/java/ch/bfh/red/app/model/test/CreateTest.java
@@ -10,29 +10,29 @@ import ch.bfh.red.app.model.profile.Patient;
 
 public class CreateTest {
 
-	@Test
-	public void test() {
+		@Test
+		public void test() {
+			
+			EntityManager em = Persistence.createEntityManagerFactory(
+					"redapp").createEntityManager();
 		
-		EntityManager em = Persistence.createEntityManagerFactory(
-				"redapp").createEntityManager();
+			Patient patient = new Patient();
+			patient.setFirstname("Bluberio");
+			patient.setName("Bluber");
+			patient.setIndependenceLevel(2);
+		
+	//		ArrayList<Task> tasks = new ArrayList<Task>();
+	//		tasks.add(task1);
+	//		tasks.add(task2);
+	//		user.setTasks(tasks);
 	
-		Patient patient = new Patient();
-		patient.setFirstname("Bluberio");
-		patient.setName("Bluber");
-		patient.setIndependenceLevel(2);
 	
-//		ArrayList<Task> tasks = new ArrayList<Task>();
-//		tasks.add(task1);
-//		tasks.add(task2);
-//		user.setTasks(tasks);
-
-
-		em.getTransaction().begin();
-		em.persist(patient);
-		em.getTransaction().commit();	
-
-		Patient patient2 = em.find(Patient.class, new Long(1));
-		System.out.println(patient2.getId());
-	}
+			em.getTransaction().begin();
+			em.persist(patient);
+			em.getTransaction().commit();	
+	
+			Patient patient2 = em.find(Patient.class, new Long(1));
+			System.out.println(patient2.getId());
+		}
 
 }
