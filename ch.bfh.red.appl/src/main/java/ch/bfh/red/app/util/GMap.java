b738ee2e4205c6715c438b294183e0998fc0fcca
stola <stola@schleppi>
2013-06-13 11:22:11 +0200

    Fix map integration

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/util/GMap.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/util/GMap.java
@@ -16,7 +16,7 @@ import com.vaadin.ui.JavaScriptFunction;
 
 @JavaScript({
 	         "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js",
-	         "gmap.js", "gmap-connector.js" })
+	         "GMap.js", "GMap-connector.js" })
 	 public class GMap extends AbstractJavaScriptComponent {
 	     
 	     public interface ValueChangeListener extends Serializable {
