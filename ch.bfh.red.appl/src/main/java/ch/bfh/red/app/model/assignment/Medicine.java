8c65dfc2f64d385907ed02c1a3933c502d945033
hammd1 <hammd1@bfh.ch>
2013-06-13 08:31:50 +0200

    comments

--- ch.bfh.red.appl/src/main/java/ch/bfh/red/app/model/assignment/Medicine.java
+++ ch.bfh.red.appl/src/main/java/ch/bfh/red/app/model/assignment/Medicine.java
@@ -13,7 +13,8 @@ import javax.persistence.OneToMany;
 
 /**
  * @author dimitri.haemmerli
- *
+ * 
+ *			Entity Medicine
  */
 @Entity
 public class Medicine {
@@ -30,34 +31,65 @@ public class Medicine {
 	private Collection<Medication> medication;
 
 
+	/**
+	 * @return the id
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * @param id
+	 * 		the id to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name
+	 * 
+	 * 		the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * @param description
+	 * 	
+	 * 		the description to set
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}	
 	
+	/**
+	 * @return medication
+	 */
 	public Collection<Medication> getMedication() {
 		return medication;
 	}
 
+	/**
+	 * @param medication
+	 * 
+	 * 		the medication to set
+	 */
 	public void setMedication(Collection<Medication> medication) {
 		this.medication = medication;
 	}
